8646fe3a9441ca8108030ef2016d227706e434ff
Sebastian <anse23@hotmail.com>
2017-04-06 18:38:09 -0500

    feat: Started to create widget

---
+++ androidapp/app/src/main/java/com/keemsa/seasonify/features/widget/SeasonWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.keemsa.seasonify.features.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by sebastian on 4/6/17.
+ */
+
+public class SeasonWidgetProvider extends AppWidgetProvider {
+}
