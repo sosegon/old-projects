79b1576de7d31a6e74740c22e6390db5664d8ee9
Sebastian <anse23@hotmail.com>
2017-04-10 22:54:43 -0500

    feat: Updated widget using an Intent Service

---
+++ androidapp/app/src/main/java/com/keemsa/seasonify/features/widget/SeasonWidgetIntentService.java
@@ -0,0 +1,75 @@
+package com.keemsa.seasonify.features.widget;
+
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.FileProvider;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.widget.RemoteViews;
+
+import com.keemsa.seasonify.R;
+import com.keemsa.seasonify.features.start.MainActivity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by sebastian on 4/6/17.
+ */
+
+public class SeasonWidgetIntentService extends IntentService {
+
+    public final static String LOG_TAG = SeasonWidgetIntentService.class.getSimpleName();
+
+    public SeasonWidgetIntentService() {
+        super(SeasonWidgetIntentService.class.getName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String predicton = pref.getString(getResources().getString(R.string.prf_prev_prediction), "");
+        String path = pref.getString(getResources().getString(R.string.prf_prev_photo), "");
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, SeasonWidgetProvider.class));
+
+        for(int appWidgetId : appWidgetIds){
+            int layoutId = R.layout.widget_season;
+
+            RemoteViews views = new RemoteViews(getPackageName(), layoutId);
+            views.setTextViewText(R.id.txt_w_season, predicton);
+
+            try {
+                InputStream input = new FileInputStream(path);
+                BitmapFactory.Options bounds = new BitmapFactory.Options();
+                Bitmap bitmap = BitmapFactory.decodeStream(input, null, bounds);
+
+                views.setImageViewBitmap(R.id.imv_w_face, bitmap);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, e.getMessage());
+            }
+
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.ll_w, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
--- androidapp/app/src/main/java/com/keemsa/seasonify/features/widget/SeasonWidgetProvider.java
+++ androidapp/app/src/main/java/com/keemsa/seasonify/features/widget/SeasonWidgetProvider.java
@@ -1,10 +1,29 @@
 package com.keemsa.seasonify.features.widget;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import com.keemsa.seasonify.R;
+import com.keemsa.seasonify.features.start.MainActivity;
 
 /**
  * Created by sebastian on 4/6/17.
  */
 
 public class SeasonWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        context.startService(new Intent(context, SeasonWidgetIntentService.class));
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, SeasonWidgetIntentService.class));
+    }
 }
