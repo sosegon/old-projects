99dee6581b9c7bf89ba7cd1d1f1a7b1faab0fdb2
Sebastian <anse23@hotmail.com>
2017-04-25 13:16:28 -0500

    feat: Processed image to detect face
    The image coming from the camera is processed to detect a face.
    Then, it is cropped around the detected face. This last image
    is the one sent to firebase.

--- androidapp/app/src/main/java/com/keemsa/seasonify/features/start/MainPresenter.java
+++ androidapp/app/src/main/java/com/keemsa/seasonify/features/start/MainPresenter.java
@@ -6,7 +6,6 @@ import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.preference.PreferenceManager;
@@ -26,7 +25,9 @@ import com.keemsa.seasonify.base.BasePresenter;
 import com.keemsa.seasonify.model.Prediction;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -37,12 +38,24 @@ import butterknife.BindArray;
 import butterknife.BindString;
 import butterknife.ButterKnife;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
 /**
  * Created by sebastian on 3/27/17.
  */
 
 public class MainPresenter extends BasePresenter<MainMvpView> implements  BitmapLoaderAsyncTask.BitmapLoaderAsyncTaskReceiver{
 
+    private final String LOG_TAG = MainPresenter.class.getSimpleName();
     private Classifier classifier;
     private Executor executor = Executors.newSingleThreadExecutor();
 
@@ -61,6 +74,9 @@ public class MainPresenter extends BasePresenter<MainMvpView> implements  Bitmap
     private FirebaseStorage mFirebaseStorage;
     private StorageReference mFacePhotoStorageReference;
 
+    private CascadeClassifier mJavaDetector;
+    private File mCascadeFile;
+
     @BindString(R.string.prf_prev_photo)
     String mPrfPrevPhotoKey;
 
@@ -78,7 +94,6 @@ public class MainPresenter extends BasePresenter<MainMvpView> implements  Bitmap
 
     @BindArray(R.array.winter_colors)
     int[] winter_colors;
-
 
     public MainPresenter(Activity activity) {
         ButterKnife.bind(this, activity);
@@ -130,41 +145,58 @@ public class MainPresenter extends BasePresenter<MainMvpView> implements  Bitmap
 
     @Override
     public void classify(final Context context, String path, Bitmap bitmap) {
-        final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+        Bitmap faceBitmap = detectFace(context, path);
 
-        if(isViewAttached()) {
+        if(faceBitmap != null){
 
-            String season = results.get(0).getTitle();
-            Uri photoUri = generateUri(context, new File(path));
-            Uri photoUri2 = Uri.fromFile(new File(path));
-            getMvpView().updateFaceView(photoUri);
-            getMvpView().updateResult(season);
-            getMvpView().updatePalette(getSeasonalColors(season));
-            storeResults(context, path, season);
+            String faceOnlyPath = new StringBuilder(path).reverse().toString();
+            faceOnlyPath = faceOnlyPath.substring(4);
+            faceOnlyPath = new StringBuilder(faceOnlyPath).reverse().toString() + "_faceonly.jpg";
 
-            final int seasonInt = getPredictionAsInteger(season);
-            StorageReference facePhotoRef = mFacePhotoStorageReference.child(photoUri.getLastPathSegment());
-            facePhotoRef.putFile(photoUri2).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                    Prediction prediction = new Prediction(seasonInt,  downloadUrl.toString());
-                    mPredictionsDatabaseReference.push().setValue(prediction);
+            try {
+                FileOutputStream out = new FileOutputStream(faceOnlyPath);
+                faceBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error saving face bitmap: " + e.getMessage());
+            }
 
-                    // Send broadcast to update the widgets
-                    Intent updateIntent = new Intent(ACTION_DATA_UPDATED);
-                    updateIntent.setPackage(context.getPackageName());
-                    context.sendBroadcast(updateIntent);
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.e("Presenter", "error when storing file");
-                }
-            });
+            final List<Classifier.Recognition> results = classifier.recognizeImage(faceBitmap);
 
+            if(isViewAttached()) {
 
+                String season = results.get(0).getTitle();
+                Uri photoUri = generateUri(context, new File(path)); // To update the view
+                Uri photoUri2 = Uri.fromFile(new File(faceOnlyPath)); // To store in firebase
+                getMvpView().updateFaceView(photoUri);
+                getMvpView().updateResult(season);
+                getMvpView().updatePalette(getSeasonalColors(season));
+                storeResults(context, path, season);
+
+                final int seasonInt = getPredictionAsInteger(season);
+                StorageReference facePhotoRef = mFacePhotoStorageReference.child(photoUri2.getLastPathSegment());
+                facePhotoRef.putFile(photoUri2).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Prediction prediction = new Prediction(seasonInt,  downloadUrl.toString());
+                        mPredictionsDatabaseReference.push().setValue(prediction);
+
+                        // Send broadcast to update the widgets
+                        Intent updateIntent = new Intent(ACTION_DATA_UPDATED);
+                        updateIntent.setPackage(context.getPackageName());
+                        context.sendBroadcast(updateIntent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(LOG_TAG, "Error when storing file: " + e.getMessage());
+                    }
+                });
+            }
+        } else {
+            // TODO Display message when no face is detected
         }
+
     }
 
     public Uri generateUri(Context context, File file) {
@@ -224,4 +256,83 @@ public class MainPresenter extends BasePresenter<MainMvpView> implements  Bitmap
         return -1;
     }
 
+    // based on https://github.com/joaopedronardari/OpenCV-AndroidSamples/blob/master/app/src/main/java/com/jnardari/opencv_androidsamples/activities/FaceDetectionActivity.java#L62
+    private BaseLoaderCallback generateLoaderCallback(final Context context) {
+        return new BaseLoaderCallback(context) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case BaseLoaderCallback.SUCCESS:
+                    {
+                        try {
+
+                            // load cascade file from application resources
+                            InputStream is = context.getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+                            File cascadeDir = context.getDir("cascade", Context.MODE_PRIVATE);
+                            mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_default.xml");
+                            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+                            while ((bytesRead = is.read(buffer)) != -1) {
+                                os.write(buffer, 0, bytesRead);
+                            }
+                            is.close();
+                            os.close();
+
+                            mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                            if (mJavaDetector.empty()) {
+                                Log.e(LOG_TAG, "Failed to load cascade classifier");
+                                mJavaDetector = null;
+                            } else
+                                Log.i(LOG_TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+                            cascadeDir.delete();
+                        } catch (IOException e) {
+                            Log.e(LOG_TAG, "Failed to load cascade: + " + e.getMessage());
+                        }
+                    } break;
+                    default:
+                    {
+                        super.onManagerConnected(status);
+                    } break;
+                }
+            }
+        };
+    }
+
+    private Bitmap detectFace(final Context context, String path) {
+        BaseLoaderCallback loaderCallback = generateLoaderCallback(context);
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(LOG_TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, context, loaderCallback);
+        } else {
+            Log.d(LOG_TAG, "OpenCV library found inside package. Using it!");
+            loaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+
+        Mat imgMAT = Imgcodecs.imread(path, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+        MatOfRect faces = new MatOfRect();
+
+        if (mJavaDetector != null){
+            mJavaDetector.detectMultiScale(imgMAT, faces);
+        }
+
+        Rect[] facesArray = faces.toArray();
+        if (facesArray.length > 0) {
+            Mat faceMAT = imgMAT.submat(facesArray[0]);
+            Mat nFaceMAT = faceMAT.clone();
+
+            Imgproc.resize(faceMAT, nFaceMAT, new Size(INPUT_SIZE, INPUT_SIZE)); // resize to pass to classifier
+            Imgproc.cvtColor(nFaceMAT, nFaceMAT, Imgproc.COLOR_RGB2BGR); // bitmap is BGR
+
+            Bitmap bmp = Bitmap.createBitmap(nFaceMAT.cols(), nFaceMAT.rows(), Bitmap.Config.RGB_565);
+            Utils.matToBitmap(nFaceMAT, bmp);
+
+            return bmp;
+        }
+
+        return null;
+    }
 }
