02997acc8c65dfeb69ee62c17f17c7f56435355e
Sebastian <anse23@hotmail.com>
2017-07-14 13:40:09 -0500

    refactor: Added application class to continue dependency injection refactoring
    The application class has been created as the root element
    in the dependency injection graph.

---
+++ androidapp/app/src/main/java/com/keemsa/seasonify/injection/component/ApplicationComponent.java
@@ -0,0 +1,30 @@
+package com.keemsa.seasonify.injection.component;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.keemsa.seasonify.SeasonifyApplication;
+import com.keemsa.seasonify.data.DataManager;
+import com.keemsa.seasonify.data.local.PreferencesHelper;
+import com.keemsa.seasonify.injection.ApplicationContext;
+import com.keemsa.seasonify.injection.module.ApplicationModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent {
+
+    void inject(SeasonifyApplication seasonifyApplication);
+
+    @ApplicationContext
+    Context context();
+
+    Application application();
+
+    PreferencesHelper preferencesHelper();
+
+    DataManager dataManager();
+}
