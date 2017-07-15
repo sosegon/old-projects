6918b8fc38e87d8f60e70fdbab179c74af0bc783
Sebastian <anse23@hotmail.com>
2017-07-15 10:20:48 -0500

    refactor: Added retrolambda to shorten code
    The code to load images have been moved out of the data manager
    and retro lambda makes it clear.

--- androidapp/app/src/main/java/com/keemsa/seasonify/data/DataManager.java
+++ androidapp/app/src/main/java/com/keemsa/seasonify/data/DataManager.java
@@ -46,84 +46,15 @@ public class DataManager {
         return mFirebaseHelper;
     }
 
-    public Observable<Bitmap> loadImage(final String path, final int imageWidth, final int imageHeight) {
-        return Observable.fromCallable(new Callable<Bitmap>() {
-            @Override
-            public Bitmap call() throws Exception {
-                InputStream input = new FileInputStream(path);
-                BitmapFactory.Options bounds = new BitmapFactory.Options();
-                Bitmap bitmap = BitmapFactory.decodeStream(input, null, bounds);
-
-                int height = bitmap.getHeight();
-                int width = bitmap.getWidth();
-
-                Matrix matrix = new Matrix();
-                matrix.postScale(imageWidth / width, imageHeight /height);
-                Bitmap scaledBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, false);
-
-                input.close();
-                return scaledBitmap;
-            }
-        }).subscribeOn(Schedulers.newThread())
+    public Observable<Bitmap> loadImage(String path, int imageWidth, int imageHeight) {
+        return Observable.fromCallable(() -> SeasonifyImage.retrieveFromFile(path, imageWidth, imageHeight))
+                .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<Bitmap> classifyImage(final String path, final int imageSize) {
+    public Observable<Bitmap> classifyImage(String path, int imageSize) {
 
-        return Observable.fromCallable(new Callable<Bitmap>() {
-            @Override
-            public Bitmap call() throws Exception {
-
-                InputStream input = new FileInputStream(path);
-                BitmapFactory.Options bounds = new BitmapFactory.Options();
-                Bitmap bitmap = BitmapFactory.decodeStream(input, null, bounds);
-
-                int rotationAngle = getCameraPhotoOrientation(path);
-                Timber.e("Rotate angle: " + rotationAngle);
-                Matrix matrix = new Matrix();
-                matrix.postRotate(rotationAngle, (float) bitmap.getWidth() / 2, (float) bitmap.getHeight() / 2);
-
-                // passed to the classifier
-                // TODO: set variable instead of 128
-                Bitmap scaledBitmap = Bitmap.createBitmap(bitmap, 0, 0, imageSize, imageSize, matrix, false);
-
-                // saved to storage
-                Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bounds.outWidth, bounds.outHeight, matrix, false);
-                SeasonifyImage.saveImage(rotatedBitmap, path);
-
-                input.close();
-
-                return scaledBitmap;
-            }
-
-            private int getCameraPhotoOrientation(String path) {
-                int rotate = 0;
-                try {
-
-                    ExifInterface exif = new ExifInterface(path);
-                    int orientation = exif.getAttributeInt(
-                            ExifInterface.TAG_ORIENTATION,
-                            ExifInterface.ORIENTATION_UNDEFINED);
-                    switch (orientation) {
-                        case ExifInterface.ORIENTATION_NORMAL:
-                            rotate = 0;
-                        case ExifInterface.ORIENTATION_ROTATE_270:
-                            rotate = 270;
-                            break;
-                        case ExifInterface.ORIENTATION_ROTATE_180:
-                            rotate = 180;
-                            break;
-                        case ExifInterface.ORIENTATION_ROTATE_90:
-                            rotate = 90;
-                            break;
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                return rotate;
-            }
-        })
+        return Observable.fromCallable(() -> SeasonifyImage.retrieveFromFileToClassify(path, imageSize))
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread());
 
